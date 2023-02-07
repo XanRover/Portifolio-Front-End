b0728f2deb4424d29d0e7425c72d66592d79f87b
XanRover <gabrielalmeida.350z@gmail.com>
2023-02-07 12:23:21 -0400

    commit update

--- src/js/js.js
+++ src/js/js.js
@@ -1,7 +1,7 @@
 const menuMobile = document.querySelector('.menu-mobile')
 const body = document.querySelector('body')
 
-menuMobile.addEventListener('click', () =>{
+menuMobile.addEventListener('click', () => {
     menuMobile.classList.contains("bi-list")
         ? menuMobile.classList.replace("bi-list", "bi-x")
         : menuMobile.classList.replace("bi-x", "bi-list");
@@ -11,12 +11,30 @@ menuMobile.addEventListener('click', () =>{
 /*fechar menu quando click*/
 const navItem = document.querySelectorAll('.nav-item')
 
-navItem.forEach(item =>{
-    item.addEventListener("click", () =>{
-        if(body.classList.contains("menu-nav-active")){
+navItem.forEach(item => {
+    item.addEventListener("click", () => {
+        if (body.classList.contains("menu-nav-active")) {
             body.classList.remove("menu-nav-active")
             menuMobile.classList.replace("bi-x", "bi-list");
         }
     })
 })
 
+/*animar os itens na tela que tenham o atributo data-anime */
+const item = document.querySelectorAll("[data-anime");
+
+const animeScroll = () => {
+    const windowTop = window.pageYOffset + innerHeight * 0.85;
+    item.forEach((element) => {
+        if (windowTop > element.offsetTop) {
+            element.classList.add("animate");
+        }else{
+            element.classList.remove("animate");
+        }
+    });
+};
+
+animeScroll();
+window.addEventListener("scroll", ()=>{
+    animeScroll();
+})
